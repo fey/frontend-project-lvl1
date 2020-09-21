2d7f31c5232e6b638f2e7dfc2cecbf97ed8b2403
Nikolay (feycot) Gagarinov <feycot@gmail.com>
2020-09-21 15:53:04 +0000

    configure eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-/* eslint-disable no-console */
-/* eslint-disable-next-line import/extensions */
 import greeting from '../src/cli.js';
 
 greeting();
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import promptly from 'promptly';
 
 export default async () => {
