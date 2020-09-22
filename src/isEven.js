a4179cc826e0e1fa5bdbaf4c4833e847c3e86999
feycot <feycot@gmail.com>
2020-09-22 21:35:59 +0500

    add randomise int

--- src/isEven.js
+++ src/isEven.js
@@ -1,14 +1,15 @@
 import promptly from 'promptly';
 
-const isEven = num => num % 2 === 0;
+const isEven = (num) => num % 2 === 0;
+const generateRandomInt = (max) => Math.floor(Math.random() * max);
 const play = async (name) => {
-
-  const number = 5;
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
-
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const maxRandomNumber = 10;
 
   for (let i = 1; i <= 3; i += 1) {
+    const number = generateRandomInt(maxRandomNumber);
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+
     console.log(`Question: ${number}`);
     const playerAnswer = await promptly.prompt('Your answer: ');
 
