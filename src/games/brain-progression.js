f68e3af794cfa01590ea3d8a52c99707ebfebc88
feycot <feycot@gmail.com>
2020-09-23 22:22:17 +0500

    fix brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -5,12 +5,19 @@ const play = async (name) => {
   console.log('What number is missing in the progression?');
 
   for (let i = 1; i <= 3; i += 1) {
-    const firstNumber = generateRandomInt(1, 20);
-    const correctAnswer = '3';
+    const firstProgressionNumber = generateRandomInt(1, 10);
+    const hiddenElementIndex = generateRandomInt(0, 9);
+    const progressionIncrementValue = generateRandomInt(1, 5);
+    const progression = (new Array(10)).fill(firstProgressionNumber, 0, 10)
+      .map((element, currentIndex) => (element + progressionIncrementValue * currentIndex));
 
-    const progressionNumbers = [1, 2, '..', 4, 5, 6];
+    const correctAnswer = progression[hiddenElementIndex].toString();
+    const question = progression
+      .slice(0, hiddenElementIndex).concat('..')
+      .concat(progression.slice(hiddenElementIndex + 1))
+      .join(' ');
 
-    console.log(`Question: ${progressionNumbers.join(' ')}`);
+    console.log(`Question: ${question}`);
     const playerAnswer = await promptly.prompt('Your answer: ');
 
     if (playerAnswer !== correctAnswer) {
